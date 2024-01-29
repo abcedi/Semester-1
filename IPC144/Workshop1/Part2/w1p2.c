c42668d993ad006dabc80500064b7dc1e1995a31
Abcedi Ilacas <145126348+abcedi@users.noreply.github.com>
2024-01-29 10:48:53 -0500

    Updated Sem1 files with laptop files

---
+++ IPC144/Workshop1/Part2/w1p2.c
@@ -0,0 +1,53 @@
+//*****************************************************************************
+//Workshop - #1 Part-2
+//Full Name : Abcedi Ilacas
+//Student ID# : 138180211
+//Email : ailacas1@myseneca.ca
+//Section : NBB
+//Authenticity Declaration :
+//I declare this submission is the result of my own work and has not been
+//shared with any other student or 3rd party content provider.This submitted
+//piece of work is entirely of my own creation.
+//******************************************************************************
+
+#include <stdio.h>  
+
+
+int main()
+{
+
+    printf("Workshop 1 Part-2\n");
+
+    printf("=================\n\n");
+
+    printf("Using tab specifiers...\n\t");
+
+    printf("Tab-1\tTab-2\tTab-3\n\t");
+
+    printf("-----\t=====\t-----\n\n");
+
+    printf("\\ this is a back-slash character!\n");
+
+    printf("%% this is a percent sign character!\n");
+
+    printf("\" this is a double-quote character!\n\n"); 
+
+    printf("My favourite quotes are:\n");
+
+    printf("1.\t\"Skill is only developed by hours and hours of work.\"\n\t\t\t\t\t\t");
+
+    printf("-Usain Bolt\n\n");
+
+    printf("2.\t\"It's not about having time. It's about making time.\"\n\t\t\t\t\t\t");
+
+    printf("-unknown\n\n");
+
+    printf("3.\t\"All of us do not have equal talent. But, all of us\n\t");
+
+    printf("have an equal opportunity to develop our talents.\"\n");
+
+    printf("\t\t\t\t\t\t-A.P.J. Abdul Kalam\n\n");
+
+    return 0;
+
+}
